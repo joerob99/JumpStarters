60fdf5690f9277ace6eaab2b13a0439c8d1e7c78
Joseph Robinson <josephnrobinson1999@gmail.com>
2021-03-29 21:32:14 -0400

    Implemented position system to track player's progress

--- Source/JumpStarters/JumpStartersPawn.h
+++ Source/JumpStarters/JumpStartersPawn.h
@@ -131,6 +131,12 @@ public:
 	UPROPERTY(Category = "RaceSystem", BlueprintReadOnly)
 	float FastestLap;
 
+	UPROPERTY(Category = "RaceSystem", BlueprintReadWrite)
+	FVector NextCheckpointLocation;
+
+	UPROPERTY(Category = "RaceSystem", BlueprintReadWrite)
+	int32 Position;
+
 	// Overlap event
 	//UFUNCTION()
 	//void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
